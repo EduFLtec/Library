943d30a626e5647143a59dcc62200614ccfa3bce
K.Korey Wertman <kkwertman@mail.fhsu.edu>
2021-12-27 16:43:51 -0500

    Refractor and comment app.js, Update readme.md project info

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+// Storage Array
 let myLibrary = [];
 
-//Element variables 
+// Element variables 
 const bookForm = document.querySelector("#book-form");
 const bookList = document.querySelector("#book-list");
 
-//Book object constructor
+// Book object constructor
 function Book (title, author, pages, read) {
         this.title = title;
         this.author = author;
@@ -21,37 +22,34 @@ Book.prototype.toggleRead = function (){
 }
 
 Book.prototype.styleReadButton = function(){
-    const bookRead = `<button class="mui-btn mui-btn--small mui-btn--primary read-button-Yes">Yes</button>`;
-    const bookNotRead = `<button class="mui-btn mui-btn--small mui-btn--primary read-button-No">No</button>`;
     if(this.read === "Yes"){
-        return bookRead;
+        return `<button class="mui-btn mui-btn--small mui-btn--primary read-button-Yes">Yes</button>`;
     } 
     if(this.read === "No"){
-        return bookNotRead;
+        return `<button class="mui-btn mui-btn--small mui-btn--primary read-button-No">No</button>`;
     }
 }
 
 function addBookToPage(book) {
         const row = document.createElement("tr");
-
-        //Insert columns
+        // Insert columns
         row.innerHTML = `
         <td class="book-data title">${book.title}</td>
         <td class="book-data author">${book.author}</td>
         <td class="book-data pages">${book.pages}</td>
         <td class="book-data read">${book.styleReadButton()}</td>
         <td><button class="mui-btn mui-btn--small mui-btn--fab mui-btn--danger delete-button">X</button></td>
-        `;
-        
+        `;  
+       
         bookList.appendChild(row);
     }
 
-//Event listener to add a book
+// Event listener to add a book
 bookForm.addEventListener('submit', function(event){
-    //Prevent page refesh on submit
+    // Prevent page refesh on submit
     event.preventDefault();
-
-    //Get form values
+   
+    // Get form values
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
@@ -59,7 +57,7 @@ bookForm.addEventListener('submit', function(event){
         
     const book = new Book(title, author, pages, read);
     
-    //Check for duplicates
+    // Check for duplicates
     if (myLibrary.some(book => book.title === title)) {
         alert("Book already in library!")
     } else {
@@ -67,32 +65,32 @@ bookForm.addEventListener('submit', function(event){
         myLibrary.push(book);
     }
 
-    //Clear form
+    // Clear form
     bookForm.reset();
 });
 
-//Use event delegation to catch dynamically generated elements
+// Use event delegation to catch dynamically generated elements
 bookList.onclick = function (event) {  
     let target = event.target;
     const targetBookTitle = target.parentNode.parentNode.firstElementChild.innerHTML;
-    // Event to change read status
-    if (target.classList.contains("read-button-Yes")){
-        const libraryBookEntry = myLibrary.find(book => book.title === targetBookTitle);
-        libraryBookEntry.toggleRead();
-        target.parentNode.innerHTML = `${libraryBookEntry.styleReadButton()}`;
-        console.log(libraryBookEntry);
+    const libraryEntry = myLibrary.find(book => book.title === targetBookTitle);
+    
+    // Event: toggle read status to "Yes"
+    if (target.classList.contains("read-button-Yes")){        
+        libraryEntry.toggleRead();
+        target.parentNode.innerHTML = `${libraryEntry.styleReadButton()}`;
     }
+    // Event: toggle read status to "No"
     if (target.classList.contains("read-button-No")) {
-        const libraryBookEntry = myLibrary.find(book => book.title === targetBookTitle);
-        libraryBookEntry.toggleRead();
-        target.parentNode.innerHTML = `${libraryBookEntry.styleReadButton()}`;
-        console.log(libraryBookEntry);
+        libraryEntry.toggleRead();
+        target.parentNode.innerHTML = `${libraryEntry.styleReadButton()}`;
     }
-    //Event to delete book
+    
+    // Event: delete book
     if (target.classList.contains("delete-button")) {
         const rowToRemove = target.parentNode.parentNode;
-        const libraryBookEntry = myLibrary.findIndex(book => book.title === targetBookTitle);        
-        myLibrary.splice(libraryBookEntry, 1); 
+        const libraryEntryIndex = myLibrary.findIndex(book => book.title === targetBookTitle);        
+        myLibrary.splice(libraryEntryIndex, 1); 
         rowToRemove.remove();
     }
 }
@@ -100,9 +98,8 @@ bookList.onclick = function (event) {
 // Starting Books 
 const book1 = new Book("The Shining", "Steven King", 780, "Yes");
 const book2 = new Book("Farenheit 451", "Ray Bradbury", 300, "No");
-
+// Add and push Starting Books
 addBookToPage(book1);
 myLibrary.push(book1);
 addBookToPage(book2);
 myLibrary.push(book2);
-
--- readme.md
+++ readme.md
@@ -1,11 +1,28 @@
 # Odin Project JavaScript Project: Library
 Assignment Details: https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/library
 
+## Live Demo
+View live demo: https://edufltec.github.io/Library/index.html
+
 ## Purpose
 > Demonstrate knowledge of:
 1. Object Orient Programing in JavaScript
     * Object Constructors
     * Protoypes and prototypical inheritance
 
-## Live Demo
-View live demo: https://edufltec.github.io/Library/index.html
+## Goals 
+* Write clean code and use DRY principle
+* Achive Purpose with ~100 lines of JavaScript
+
+## Things Learned
+* Differences between find() and findIndex(): https://webdevidea.com/blog/difference-between-find-and-findindex-in-javascript/
+* How to debug splice()
+* splice() requires an index value
+* How to event delegation to catch dynamically generated HTML elements
+
+## Built with 
+1. HTML
+2. JavaScript  
+3. CSS
+4. MUI: a lightweight CSS framework that follows Google's Material Design guidelines(https://www.muicss.com); 
+    * Helpful in building a clean and effective UI
